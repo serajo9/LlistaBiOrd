a953139ad7b40bdaa9b50e856c2bf4099839eebe
Sergi <serajo99@gmail.com>
2018-10-06 17:34:49 +0200

    feat: Added all functions
    All the functions of the list are now added.

--- llista.h
+++ llista.h
@@ -15,3 +15,18 @@ typedef struct {
     Node *ult;
     Node *pdi;
 } ListaBiOrd;
+
+ListaBiOrd LISTABIORD_crea ();
+void LISTABIORD_afegirOrdenat(ListaBiOrd l, int e);
+ListaBiOrd LISTABIORD_elimina (ListaBiOrd l);
+int LISTABIORD_consulta (ListaBiOrd l);
+int LISTABIORD_buida (ListaBiOrd l);
+int LISTABIORD_fi (ListaBiOrd l);
+int LISTABIORD_inici (ListaBiOrd l);
+ListaBiOrd LISTABIORD_vesInici (ListaBiOrd l);
+ListaBiOrd LISTABIORD_vesFi (ListaBiOrd l);
+ListaBiOrd LISTABIORD_avanca (ListaBiOrd l);
+ListaBiOrd LISTABIORD_retrocedeix (ListaBiOrd l);
+ListaBiOrd LISTABIORD_destrueix (ListaBiOrd l);
+
+#endif
