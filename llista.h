6b6021c0ad6a36cd7643cc8302734ff7880fed50
Sergi <serajo99@gmail.com>
2018-10-06 17:27:47 +0200

    feat: Added libraries and structs
    Added all the necessary libraries and structs to the file.

--- llista.h
+++ llista.h
@@ -1,1 +1,17 @@
+#ifndef _LISTA_BIORD_H
+#define _LISTA_BIORD_H
 
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct N {
+    int e;
+    struct N *sig;
+    struct N *ant;
+} Node;
+
+typedef struct {
+    Node *pri;
+    Node *ult;
+    Node *pdi;
+} ListaBiOrd;
