c90a8516c9d0b5e1433f1d2159f6cfc03fb0277d
Sergi <serajo99@gmail.com>
2018-10-06 17:46:49 +0200

    feat: Added a list create function
    Created a new function that creates a new list from zero.

--- llista.c
+++ llista.c
@@ -1,1 +1,29 @@
+#include "llista.h"
 
+ListaBiOrd LISTABIORD_crea(){
+    ListaBiOrd l;
+
+    l.pri = NULL;
+    l.pri = (Node*)malloc(sizeof(Node));
+    if (l.pri == NULL){
+        printf("Error al reservar memoria.\n")
+    }
+    else {
+        l.ult = NULL;
+        l.ult = (Node*)malloc(sizeof(Node));
+        if (l.ult == NULL){
+            printf ("Error al reservar memoria.\n");
+        }
+        else{
+            l.pri->sig = l.ult;
+            l.pri->ant = NULL;
+
+            l.ult->sig = NULL;
+            l.ult->ant = l.pri;
+
+            l.pdi = l.pri;
+        }
+    }
+
+    return l;
+}
