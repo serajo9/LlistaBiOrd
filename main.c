8de750bd3cd0607e0c39abeff3e3db5f590b6598
Sergi <serajo99@gmail.com>
2018-10-07 22:39:14 +0200

    feat: main.c finished
    File named main.c is now finished so we can prove if the list works
    correctly.

--- main.c
+++ main.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+#include "llista.h"
+
+void main(){
+
+    ListaBiOrd l;
+    int e = 0;
+
+    l = LISTABIORD_crea();
+    void LISTABIORD_afegirOrdenat(l, e);
+
+    e = 2;
+    void LISTABIORD_afegirOrdenat(l, e;
+
+    e = 1;
+    void LISTABIORD_afegirOrdenat(l, e);
+
+    while (LISTABIORD_fi != 1){
+        LISTABIORD_avanca(l);
+    }
+
+    l = LISTABIORD_destrueix(l);
+}
